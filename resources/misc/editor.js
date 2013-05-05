c59dc54fc89aa6547a6f139188e05191458ff0e7
Mads Flensted-Urech <mfu@coadu.com>
2013-05-05 12:55:52 +0200

    Make help text markdown string

--- resources/misc/editor.js
+++ resources/misc/editor.js
@@ -16,9 +16,15 @@ function setEditorBottom() {
   editor.refresh();
 }
 
+
 function initHelpView() {
-  var text = "Helpfull text";
-  var help_div = generateView(text, false, 'doc');
+  var help_text = "###Shortcut keys \n" +
+  "- `Ctrl+Enter`: Compile code. Tick 'Autocompile' to compile on changes. \n" +
+  "- `Ctrl+K`: Show documentation for token under (left-of) cursor. \n" +
+  "- `Ctrl+Shift+K`: Open documentation page in new Tab/Window. \n" +
+  "\n\n<input type='button' value='Close' onclick='hideHelpView();'/>";
+  var markdown = new Showdown.converter();
+  var help_div = generateView(markdown.makeHtml(help_text), true, 'doc');
   var helpView = document.getElementById('help_text');
   helpView.appendChild(help_div);
 }
